d9774b4abed9aa3f5564e87ce7bbd750701c6579
Wellington Arantes <w.aranteslfc@gmail.com>
2024-11-24 01:48:09 -0300

    chore: update make-question test factory

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,3 +1,5 @@
+import { faker } from '@faker-js/faker'
+
 import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import {
   Question,
@@ -5,14 +7,20 @@ import {
 } from '@/domain/forum/enterprise/entities/question'
 import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    authorId: new UniqueEntityId(),
-    title: 'example title',
-    content: 'example content',
-    slug: Slug.create('example-question'),
-    ...override,
-  })
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityId,
+) {
+  const question = Question.create(
+    {
+      authorId: new UniqueEntityId(),
+      title: faker.lorem.sentence(),
+      content: faker.lorem.text(),
+      slug: Slug.create('example-question'),
+      ...override,
+    },
+    id,
+  )
 
   return question
 }
