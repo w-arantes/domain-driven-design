b3342ce2a7b55420a2171b8453fed20fffd7334c
Wellington Arantes <w.aranteslfc@gmail.com>
2024-11-24 22:33:05 -0300

    feat: delete-question use case

---
+++ src/domain/forum/application/use-cases/delete-question.spec.ts
@@ -0,0 +1,54 @@
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { DeleteQuestionUseCase } from './delete-question'
+import { makeQuestion } from 'test/factories/make-question'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+
+describe('Delete Question', () => {
+  let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+  let sut: DeleteQuestionUseCase
+
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new DeleteQuestionUseCase(inMemoryQuestionsRepository)
+  })
+
+  it('should be able to delete a question', async () => {
+    const fakeQuestionAuthor = new UniqueEntityId('author-1')
+    const newQuestionId = 'question-1'
+    const newQuestion = makeQuestion(
+      {
+        authorId: fakeQuestionAuthor,
+      },
+      new UniqueEntityId(newQuestionId),
+    )
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    await sut.execute({
+      questionId: newQuestionId,
+      authorId: fakeQuestionAuthor.toString(),
+    })
+
+    expect(inMemoryQuestionsRepository.items).toHaveLength(0)
+  })
+
+  it('should not be able to delete a question from another user', async () => {
+    const fakeQuestionAuthor = new UniqueEntityId('author-1')
+    const newQuestionId = 'question-1'
+    const newQuestion = makeQuestion(
+      {
+        authorId: fakeQuestionAuthor,
+      },
+      new UniqueEntityId(newQuestionId),
+    )
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    expect(() => {
+      return sut.execute({
+        questionId: newQuestionId,
+        authorId: 'author-2',
+      })
+    }).rejects.toBeInstanceOf(Error)
+  })
+})
---
+++ src/domain/forum/application/use-cases/delete-question.ts
@@ -0,0 +1,31 @@
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
+
+interface DeleteQuestionUseCaseRequest {
+  authorId: string
+  questionId: string
+}
+
+interface DeleteQuestionUseCaseResponse {}
+
+export class DeleteQuestionUseCase {
+  constructor(private questionsRepository: QuestionsRepository) {}
+
+  async execute({
+    authorId,
+    questionId,
+  }: DeleteQuestionUseCaseRequest): Promise<DeleteQuestionUseCaseResponse> {
+    const question = await this.questionsRepository.findById(questionId)
+
+    if (!question) {
+      throw new Error('Question not found.')
+    }
+
+    if (authorId !== question.authorId.toString()) {
+      throw new Error('Not allowed to delete this question.')
+    }
+
+    await this.questionsRepository.delete(question)
+
+    return {}
+  }
+}
