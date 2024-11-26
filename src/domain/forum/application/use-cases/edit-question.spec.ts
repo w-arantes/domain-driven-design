27c5ddf5088c2b27283e4fbf2ac79e6580ba2c15
Wellington Arantes <w.aranteslfc@gmail.com>
2024-11-26 19:35:35 -0300

    feat: edit-question use case

---
+++ src/domain/forum/application/use-cases/edit-question.spec.ts
@@ -0,0 +1,61 @@
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { EditQuestionUseCase } from './edit-question'
+import { makeQuestion } from 'test/factories/make-question'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+
+describe('Edit Question', () => {
+  let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+  let sut: EditQuestionUseCase
+
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new EditQuestionUseCase(inMemoryQuestionsRepository)
+  })
+
+  it('should be able to edit a question', async () => {
+    const fakeQuestionAuthor = new UniqueEntityId('author-1')
+    const newQuestionId = 'question-1'
+    const newQuestion = makeQuestion(
+      {
+        authorId: fakeQuestionAuthor,
+      },
+      new UniqueEntityId(newQuestionId),
+    )
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    await sut.execute({
+      questionId: newQuestionId,
+      authorId: fakeQuestionAuthor.toString(),
+      title: 'New Title',
+      content: 'New Content',
+    })
+
+    expect(inMemoryQuestionsRepository.items[0]).toMatchObject({
+      title: 'New Title',
+      content: 'New Content',
+    })
+  })
+
+  it('should not be able to edit a question from another user', async () => {
+    const fakeQuestionAuthor = new UniqueEntityId('author-1')
+    const newQuestionId = 'question-1'
+    const newQuestion = makeQuestion(
+      {
+        authorId: fakeQuestionAuthor,
+      },
+      new UniqueEntityId(newQuestionId),
+    )
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    expect(() => {
+      return sut.execute({
+        questionId: newQuestionId,
+        authorId: 'author-2',
+        title: 'New Title',
+        content: 'New Content',
+      })
+    }).rejects.toBeInstanceOf(Error)
+  })
+})
