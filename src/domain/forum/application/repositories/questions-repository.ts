bef37c9d592388bc907d4fb3cf9d7f7ecffdff7e
Wellington Arantes <w.aranteslfc@gmail.com>
2024-11-28 23:34:45 -0300

    feat: fetch recent questions use case

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -1,8 +1,10 @@
 import { Question } from '@/domain/forum/enterprise/entities/question'
+import { PaginationParams } from '@/core/repositories/pagination-params'
 
 export interface QuestionsRepository {
   findById(id: string): Promise<Question | null>
   findBySlug(slug: string): Promise<Question | null>
+  findManyRecent(params: PaginationParams): Promise<Question[]>
   save(question: Question): Promise<void>
   create(question: Question): Promise<void>
   delete(question: Question): Promise<void>
---
+++ src/domain/forum/application/use-cases/fetch-recent-questions.ts
@@ -0,0 +1,24 @@
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
+import { Question } from '@/domain/forum/enterprise/entities/question'
+
+interface FetchRecentQuestionsUseCaseRequest {
+  page: number
+}
+
+interface FetchRecentQuestionsUseCaseResponse {
+  questions: Question[]
+}
+
+export class FetchRecentQuestionsUseCase {
+  constructor(private questionsRepository: QuestionsRepository) {}
+
+  async execute({
+    page,
+  }: FetchRecentQuestionsUseCaseRequest): Promise<FetchRecentQuestionsUseCaseResponse> {
+    const questions = await this.questionsRepository.findManyRecent({ page })
+
+    return {
+      questions,
+    }
+  }
+}
