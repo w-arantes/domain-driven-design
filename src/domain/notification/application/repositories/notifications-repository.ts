cc3bd6d97d32be88e4f2697d31ab264789096058
Wellington Arantes <w.aranteslfc@gmail.com>
2024-12-19 18:57:48 -0300

    feat: send notification use case

---
+++ src/domain/notification/application/repositories/notifications-repository.ts
@@ -0,0 +1,5 @@
+import { Notification } from '@/domain/notification/enterprise/entities/notification'
+
+export class NotificationsRepository {
+  async create(notification: Notification): Promise<void> {}
+}
