8fb4efc7e13a7ee0c8f5a1dc7aefc0858f01b125
Wellington Arantes <w.aranteslfc@gmail.com>
2025-01-08 20:22:52 -0300

    feat: add domain events tests

---
+++ src/core/events/domain-events.spec.ts
@@ -0,0 +1,46 @@
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { AggregateRoot } from '@/core/entities/aggregate-root'
+
+import { DomainEvent } from './domain-event'
+import { DomainEvents } from './domain-events'
+
+class CustomAggregateCreated implements DomainEvent {
+  public occurredAt: Date
+  private aggregate: CustomAggregate //eslint-disable-line
+
+  constructor(aggregate: CustomAggregate) {
+    this.occurredAt = new Date()
+    this.aggregate = aggregate
+  }
+
+  public getAggregateId(): UniqueEntityId {
+    return this.aggregate.id
+  }
+}
+
+class CustomAggregate extends AggregateRoot<null> {
+  static create() {
+    const aggregate = new CustomAggregate(null)
+
+    aggregate.addDomainEvent(new CustomAggregateCreated(aggregate))
+
+    return aggregate
+  }
+}
+
+describe('domain events', () => {
+  it('should be able to dispatch and listen to events', () => {
+    const callbackSpy = vi.fn()
+
+    DomainEvents.register(callbackSpy, CustomAggregateCreated.name)
+    
+    const aggregate = CustomAggregate.create()
+    
+    expect(aggregate.domainEvents).toHaveLength(1)
+
+    DomainEvents.dispatchEventsForAggregate(aggregate.id)
+
+    expect(callbackSpy).toHaveBeenCalled()
+    expect(aggregate.domainEvents).toHaveLength(0)
+  })
+})
